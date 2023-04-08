327852f0f7bec752b823412e61dff54b42f0ecdb
咕谷酱 <74496778+GooGuJiang@users.noreply.github.com>
2023-04-08 07:14:47 +0000

    🔨添加页面标题

--- src/components/TopNavbar.tsx
+++ src/components/TopNavbar.tsx
@@ -18,19 +18,11 @@ function HcatNavbar() {
         HCAT
       </Text>
         </Navbar.Brand>
-        <Navbar.Content hideIn="xs">
-          <Navbar.Link href="#">Features</Navbar.Link>
-          <Navbar.Link href="#">Customers</Navbar.Link>
-          <Navbar.Link href="#">Pricing</Navbar.Link>
-          <Navbar.Link href="#">Company</Navbar.Link>
-        </Navbar.Content>
+          
         <Navbar.Content>
-          <Navbar.Link color="inherit" href="#">
-            登录
-          </Navbar.Link>
           <Navbar.Item>
-            <Button disabled auto flat as={Link} href="#">
-              注册
+            <Button auto flat as={Link} href="https://docs.hcat.online/">
+              文档
             </Button>
           </Navbar.Item>
         </Navbar.Content>
